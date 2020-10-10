c027d30478fc3a6dffa01aa21bdf8c174e6adbe2
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2020-10-10 13:31:24 -0400

    Update a2q1.py

--- 2-loot-boxes/a2q1.py
+++ 2-loot-boxes/a2q1.py
@@ -30,20 +30,21 @@ def printLootbox():
     print("\nPlease select a loot box from the menu below:\n")
     i = 0
     while i < boxCount:
-        print(str(i + 1) + ". " + "[" + boxRarity[i] + "]" + " (" + str(boxPrices[i]) + ") " + boxNames[i])
+        print(str(i + 1) + ". " + "[" + boxRarity[i] + "]" + " (" + str('{:4.2f}'.format(boxPrices[i])) + ") " + boxNames[i])
         i += 1
 
 def errorWrongInput():
     print("\nError: That was not a valid selection. Please enter a number between 1-" + str(boxCount))
 
 def receipt():
-    total = float((int(quantity) * float(boxPrices[updatedValue])))
+    total = float((int(quantity) * float('{:4.2f}'.format(boxPrices[updatedValue]))))
     print("---------------------------------")
-    print(str(quantity) + "x " + boxNames[updatedValue] + " ($" + str(boxPrices[updatedValue]) + ")")
+    print(str(quantity) + "x " + boxNames[updatedValue] + " ($" + str('{:4.2f}'.format(boxPrices[updatedValue])) + ")")
     print("---------------------------------")
     print ("\nTotal Cost: $" + str(total))
     print("Thank you! Good luck, gamer!\n")
 
+# print ('{:4.2f}'.format'{:4.2f}'.format(boxPrices[0]))
 
 # game start
 gamerName = input("\nHELLO, GAMER! Welcome to the Raven Runner Loot Box Purchasing System. First, what's your player name?\n")
@@ -66,7 +67,7 @@ while selectionMade == False:
 updatedValue = int(selection) +- 1
 finalized = False
 while finalized == False:
-    quantity = input("\nHow many of " + boxNames[updatedValue] + "s (" + str(boxPrices[updatedValue]) + ") would you like to purchase? ")
+    quantity = input("\nHow many of " + boxNames[updatedValue] + "s (" + str('{:4.2f}'.format(boxPrices[updatedValue])) + ") would you like to purchase? ")
     isDigit = quantity.isdigit()
     if isDigit == False:
         print("\nThat is not a quantity. Please type a number of items you wish to purchase.")
