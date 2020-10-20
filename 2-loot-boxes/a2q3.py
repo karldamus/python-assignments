318069f82d9540c2f9a93af18fafb1b2152bd70a
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2020-10-20 12:26:21 -0400

    Update a2q3.py

--- 2-loot-boxes/a2q3.py
+++ 2-loot-boxes/a2q3.py
@@ -45,10 +45,83 @@ while orderComplete == False:
         if(int(userBox) == int(4)):
             orderComplete = True
         elif((int(userBox) >= 1) and (int(userBox) <= 3)):
-            cart.append(userBox)
+            updatedValue = int(int(userBox) - 1) # update the value to associate with list value
+            itemDisplayText = str(boxNames[updatedValue])
+            # ensure no false inputs for the quantity
+            falseQuantity = True
+            while falseQuantity == True:
+                print("How many of " + itemDisplayText + " (" + str(boxPrices[updatedValue]) + ") "  "would you like to purchase?")
+                quantity = input()
+                if quantity.isdigit():
+                    if(int(quantity) > 0):
+                        # do calc and push to array then set falseQuantity to False
+                        quantitativePush = 0
+                        while quantitativePush < int(quantity):
+                            cart.append(updatedValue)
+                            quantitativePush += 1
+                        falseQuantity = False
+                else:
+                    print("\nThat is not a valid quantity. Please enter any number above 0")
         else:
             print("\nThat is not a listed box. Please enter a number between 1 and 3")
     else:
         print("\nThat is not a valid number. Please enter a number between 1 and 3")
 
-print(sorted(cart))
+# calculations of boxes
+finalCart = sorted(cart)
+commons = 0
+rares = 0
+epics = 0
+
+for i in finalCart:
+    if i == int(0):
+        commons += 1
+    if i == int(1):
+        rares += 1
+    if i == int(2):
+        epics += 1
+
+totalCost = ((commons * boxPrices[0]) + (rares * boxPrices[1]) + (epics * boxPrices[2]))
+
+# print("Common: " + str(commons))
+# print("Rare: " + str(rares))
+# print("Epic: " + str(epics))
+
+
+
+# user has finished their order let's print the lootboxes
+print("\nThanks, Test! Here is your receipt:")
+print("---------------------------------")
+if commons > 0:
+    print(str(commons) + "x" + "\t" + boxNames[0] + " (" + str(boxPrices[0]) + ")")
+if rares > 0:
+    print(str(rares) + "x" + "\t" + boxNames[1] + " (" + str(boxPrices[1]) + ")")
+if epics > 0:
+    print(str(epics) + "x" + "\t" + boxNames[2] + " (" + str(boxPrices[2]) + ")")
+print("---------------------------------")
+print ("\nTotal Cost: $" + str('{:4.2f}'.format(totalCost)))
+print("Thank you! Good luck, gamer!\n")
+
+
+# loop through each item in cart
+# for i in finalCart:
+
+
+
+
+
+
+
+# print(cart)
+
+
+
+# if common > 0:
+#     print(str(common) + "x " + boxNames[0] + " ($" + str(boxPrices[0]) + ")")
+#     total += (int(common) * float(boxPrices[0]))
+# if rare > 0:
+#     print(str(rare) + "x " + boxNames[1] + " ($" + str(boxPrices[1]) + ")")
+#     total += (int(rare) * float(boxPrices[1]))
+# if epic > 0:
+#     print(str(epic) + "x " + boxNames[2] + " ($" + str(boxPrices[2]) + ")")
+#     total += (int(epic) * float(boxPrices[2]))
