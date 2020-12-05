bb577e94f1f0aa45607cfa34cee4e19ea3693af4
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2020-12-05 17:48:17 -0500

    FInished question1.py

---
+++ 5-recursion-sorting-pathfinding/question1.py
@@ -0,0 +1,35 @@
+# Recursion, Sorting, & Pathfinding
+# Question 1: Recursive Cached Fibonacci
+# 
+# ©2020 Karl Damus, All Rights Reserved
+#
+import time
+''' 0, 1, 1, 2, 3, 5, 8, 13, 21... '''
+testDict = {1:1, 2:1, 3:2, 4:3, 5:5}
+
+def main():
+
+    print(cachedfibonacci(6, cache={})) # expected output: 8
+    print(cachedfibonacci(10, cache={})) # expected output: 55
+    print(cachedfibonacci(100, cache={})) # expected output: 354,224,848,179,261,915,075
+
+    ''' just for fun: comment this out to see what the 100'000th digit of the fibonacci sequence is '''
+    # x = 100000
+    # for i in range(x):
+    #     cachedfibonacci(i, cache={})
+    # print(cachedfibonacci(x, cache={}))
+
+def cachedfibonacci(n, cache={}):
+    # Base Cases
+    if n == 1 or n == 2:
+        return 1
+    elif n == 0:
+        return 0
+    # Recursive Calculation
+    elif n not in cache:
+        cache[n] = cachedfibonacci(n-1) + cachedfibonacci(n-2)
+    
+    return cache[n]
+
+if __name__ == "__main__":
+    main()
