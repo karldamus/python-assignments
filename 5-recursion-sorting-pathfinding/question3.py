3bab68526e622ff8d52e9e45039e3496cf0b376f
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2020-12-07 17:24:04 -0500

    Update question3.py

--- 5-recursion-sorting-pathfinding/question3.py
+++ 5-recursion-sorting-pathfinding/question3.py
@@ -16,12 +16,59 @@ with a valid path from the start to end. It does not need to be the shortest pat
 from data import maze_helper as mh
 
 maze = mh.sample_maze()
-mh.print_maze(maze)
+# mh.print_maze(maze)
 
 wall = "#"
 player = "O"
 end = "X"
 space = " "
 
+def main():
+    current_point = start_point()
+    connected_points = mh.get_adjacent_positions(maze, current_point)
+    dfs(connected_points, current_point, explored=[])
+    # print("The current point is: " + str(current_point) + ". The connected points are: " + str(connected_points))
 
+def return_error(message):
+    print(message)
 
+def start_point():
+    # reset current list to search in
+    current_list = 0
+    # go through each list inside the maze list
+    for lists in maze:
+        for i in range(len(lists)):
+            # if "O" is the value, it is the start position!
+            if lists[i] == "O":
+                return_point = (current_list, i)
+                return return_point
+        # fall back if no point found
+        return_error("ERROR 1: Start point not found")
+        current_list += 1
+    
+# print(maze)
+def dfs(connected_points, current_point, explored=[]):
+    endingFound = False
+    explored.append(current_point)
+    # print("The current explored points are: " + str(explored))
+    for new_current_point in connected_points:
+        for i in new_current_point:
+            isend = mh.symbol_at(maze, current_point)
+        if isend == "X":
+            print("Ending Found!")
+            endingFound = True
+            print("The current point is: " + str(current_point))
+            print("The explored pathways are: " + str(explored))
+            break
+
+        if new_current_point not in explored and endingFound == False:
+            explored.append(new_current_point)
+            new_connected_points = mh.get_adjacent_positions(maze, new_current_point)
+            dfs(new_connected_points, new_current_point, explored)
+    # if endingFound == False:
+    #     print("No ending was found. Maybe it's an impossible maze OoO")
+        
+    # print(explored)
+
+if __name__ == '__main__':
+    main()
