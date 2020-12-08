4ed0fae476ee3d1c3432e95712129a3006af4edc
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2020-12-08 18:43:28 -0500

    update files

--- 5-recursion-sorting-pathfinding/question2.py
+++ 5-recursion-sorting-pathfinding/question2.py
@@ -3,20 +3,6 @@
 # 
 # ©2020 Karl Damus, All Rights Reserved
 #
-'''
-Write a function, ​closest_enemies(tuple(int,int), list(tuple(int,int)))​. 
-This signature is a bit complex, 
-but really, it could look as simple as 
-​def closest_enemies(hero_position, enemy_positions):
-
-As parameters, the function accepts:
-    ● A tuple, containing two integers, 
-    representing the x, y coordinate of our player character
-        ○ Ex. (50, 10), (20, 35)
-    ● A list of tuples, each tuple containing two integers, 
-    representing enemy positions
-        ○ Ex. [ (10, 20), (55, 10), (23, -5), (0, 200) ]
-'''
 
 import math
 
--- 5-recursion-sorting-pathfinding/question3.py
+++ 5-recursion-sorting-pathfinding/question3.py
@@ -24,22 +24,12 @@ line is going through #? Why is this
 from data import maze_helper as mh
 
 maze = mh.sample_maze()
-# mh.print_maze(maze)
-
-wall = "#"
-player = "O"
-end = "X"
-space = " "
 
 def main():
     current_point = start_point()
     connected_points = mh.get_adjacent_positions(maze, current_point)
-    print(dfs(connected_points, current_point, explored=[]))
+    dfs(connected_points, current_point, explored=[])
     mh.print_maze(maze)
-    # for i in maze:
-    #     print(i)
-    # print(route)
-    # draw_path(route)
 
 def return_error(message):
     print(message)
@@ -63,7 +53,6 @@ def dfs(connected_points, current_point, explored=[]):
     explored.append(current_point)
 
     for test_point in connected_points:
-        # print(test_point)
         for stringValue in test_point:
             isend = mh.symbol_at(maze, current_point)
             if isend == "X":
@@ -74,45 +63,14 @@ def dfs(connected_points, current_point, explored=[]):
         endingFound = False
         if test_point not in explored and endingFound != True:
             new_point = test_point # rename for clarity
-            # explored.append(new_point)
             new_connected_points = mh.get_adjacent_positions(maze, new_point)
-            print(new_connected_points)
             dfs(new_connected_points, new_point, explored)
-    
-    # try:
-    #     if endingFound == True:
-    #         for route_point in explored:
-    #             x = route_point[1]
-    #             y = route_point[0]
-    #             maze[y][x] = "."
-    # except:
-    #     pass
 
 def draw_route(explored):
-    print(explored)
     for route_point in explored:
         x = route_point[1]
         y = route_point[0]
         maze[y][x] = "."
-
-    
-    # try:
-    #     print(explorationOutput)
-    #     return explorationOutput
-    # except:
-    #     pass
-    # if endingFound == False:
-    #     print("No ending was found. Maybe it's an impossible maze OoO")
-        
-    # print(explored)
-
-def draw_path(finalized_exploration):
-    # for v in range(len(finalized_exploration)):
-    #     print("test")
-    #     print(point)
-    #     print(point_to_draw)
-    pass
-
 
 if __name__ == '__main__':
     main()
